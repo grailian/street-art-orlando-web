3048fb69e98b328e3ea42641de33ae22aff18cdf
Cassandra Wilcox <csndra0504@gmail.com>
2016-03-30 15:20:45 +0200

    testing web pack bundling

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log('webpack');
+
+//JS NODE_MODULES
+require('../node_modules/angular/angular.js')
+require('../node_modules/angular-ui-router/release/angular-ui-router.min.js')
+
+//JS APP SCRIPTS
+require('../src/index.js')
+require('../src/index.routes.js')
+
+require('../src/views/Home/HomeCtrl.js')
+require('../src/views/Login/LoginCtrl.js')
+require('../src/views/Register/RegisterCtrl.js')
+require('../src/views/Forgot/ForgotCtrl.js')
