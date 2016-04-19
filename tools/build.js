42ff36dafbce1f73e37557be238060d71564b0c6
Cassandra Wilcox <csndra0504@gmail.com>
2016-04-18 21:35:24 -0400

    fix: catch build errors

--- tools/build.js
+++ tools/build.js
@@ -3,5 +3,6 @@ console.log('build')
 var spawn = require('child_process').spawn;
 var child = spawn('webpack', ['--colors']);
 child.stdout.pipe(process.stdout);
+child.stderr.pipe(process.stderr);
 
 module.exports = child;
