f4d1b216441beed9d37dbb7e32e747a3a6d2fb11
Ian Grail <grailians@gmail.com>
2016-06-07 19:47:44 -0400

    fix: local server opn

--- tools/start.js
+++ tools/start.js
@@ -7,13 +7,10 @@ var child = require('./build');
 child.stdout.on('close', function(chunk) {
 
   require('../server.js');
+  opn('http://localhost:8000');
 
   var child3 = spawn('webpack', ['--colors', '--watch']);
   child3.stdout.pipe(process.stdout);
   child3.stderr.pipe(process.stderr);
 
 });
-
-setTimeout(function() {
-  opn('http://localhost:8000');
-}, 2000);
