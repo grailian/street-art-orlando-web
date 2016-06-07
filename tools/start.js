3d8ba3894b44f8c65a06f5a22d59b057e43f3446
Ian Grail <grailians@gmail.com>
2016-06-07 19:46:01 -0400

    fix: local server to work with html5 push states

--- tools/start.js
+++ tools/start.js
@@ -3,10 +3,17 @@ console.log('start');
 var spawn = require('child_process').spawn;
 var opn = require('opn');
 
-var child1 = spawn('webpack-dev-server', ['--port', '8081', '--colors']);
-child1.stdout.pipe(process.stdout);
-child1.stderr.pipe(process.stderr);
+var child = require('./build');
+child.stdout.on('close', function(chunk) {
+
+  require('../server.js');
+
+  var child3 = spawn('webpack', ['--colors', '--watch']);
+  child3.stdout.pipe(process.stdout);
+  child3.stderr.pipe(process.stderr);
+
+});
 
 setTimeout(function() {
-  opn('http://localhost:8081');
+  opn('http://localhost:8000');
 }, 2000);
