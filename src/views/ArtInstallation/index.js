fd0a9f2903a23a9db6736c6e5e0db51f069c0c20
Ian Grail <grailians@gmail.com>
2016-06-08 00:04:54 -0400

    feat: add google map to detail view

--- src/views/ArtInstallation/index.js
+++ src/views/ArtInstallation/index.js
@@ -22,6 +22,29 @@
         });
     };
 
+    vm.setMap = function(installation) {
+      var myLatLng = {
+        lat: installation.location.latitude,
+        lng: installation.location.longitude
+      };
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 14,
+        disableDefaultUI: true,
+        zoomControl: true,
+        scaleControl: false
+      });
+
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Art Spotted!'
+      });
+
+      console.log("map", map)
+    };
+
     vm.getInstallation = function(id) {
       ArtService.getInstallation(id)
         .success(function(data) {
@@ -29,6 +52,7 @@
           console.log("vm.installation", vm.installation)
           vm.getArtist(vm.installation.creator.objectId);
           vm.getNearbyInstallations(vm.installation.location);
+          vm.setMap(vm.installation);
         })
         .error(function(err) {
           console.error("err", err)
@@ -38,7 +62,7 @@
     vm.getNearbyInstallations = function(location) {
       ArtService.getInstallations(location)
         .success(function(data) {
-          vm.installations = data.results.slice(0,3);
+          vm.installations = data.results.slice(0, 3);
           console.log("vm.installations", vm.installations)
         })
         .error(function(err) {
